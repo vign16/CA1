b1e5dab2dbbc10746743a3a0a428f876c8fce8f0
ajaykumar0506 <b.kumar2@s.amity.edu>
2025-02-25 12:10:37 +0530

    seventh commit simple intrest using float removed comments

--- simpleintrest.c
+++ simpleintrest.c
@@ -11,8 +11,6 @@ int main() {
 
     printf("Time: ");
     scanf("%f", &time);
-
-    // Calculate interest
     interest = principal * rate * time / 100;
 
     printf("Simple Interest: %.2f\n", interest);

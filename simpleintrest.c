a6c00e9750fc3aa0c026742b323e71ad18c288f1
ajaykumar0506 <b.kumar2@s.amity.edu>
2025-02-25 11:56:48 +0530

    fourth commit simple intrest using float

--- simpleintrest.c
+++ simpleintrest.c
@@ -1,16 +1,31 @@
-include <stdio.h>
+#include <stdio.h>
+
+// Function to calculate simple interest
+float calculateSimpleInterest(float principal, float rate, float time) {
+    return (principal * rate * time) / 100;
+}
 
 int main() {
     float principal, rate, time, interest;
+
+    // Ask the user to enter the principal amount, rate of interest, and time period
     printf("Enter the principal amount: ");
     scanf("%f", &principal);
+
     printf("Enter the rate of interest (in percentage): ");
     scanf("%f", &rate);
+
     printf("Enter the time period (in years): ");
     scanf("%f", &time);
-    interest = (principal * rate * time) / 100;
+
+    // Calculate simple interest by calling the function
+    interest = calculateSimpleInterest(principal, rate, time);
+
+    // Display the result
     printf("The simple interest is: %.2f\n", interest);
+
     return 0;
 }
+
 
 

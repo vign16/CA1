a159959d0d7420faa4ed18c8b307de00ec23831c
ajaykumar0506 <b.kumar2@s.amity.edu>
2025-02-25 11:43:50 +0530

    Second commit simple intrest removed comments

--- simpleintrest.c
+++ simpleintrest.c
@@ -2,23 +2,14 @@
 
 int main() {
     float principal, rate, time, interest;
-
-    // Ask the user to enter the principal amount, rate of interest, and time period
     printf("Enter the principal amount: ");
     scanf("%f", &principal);
-
     printf("Enter the rate of interest (in percentage): ");
     scanf("%f", &rate);
-
     printf("Enter the time period (in years): ");
     scanf("%f", &time);
-
-    // Calculate simple interest
     interest = (principal * rate * time) / 100;
-
-    // Display the result
     printf("The simple interest is: %.2f\n", interest);
-
     return 0;
 }
 
